8eec55c4e7c17bcb90ce76bfe7fce8c09ae0d362
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2024-03-29 12:26:17 -0400

    Add format command

--- init.lua
+++ init.lua
@@ -234,6 +234,20 @@ _G.reset_hunk = function()
 end
 vim.api.nvim_set_keymap('n', '<C-g>r', '<cmd>lua reset_hunk()<CR>', { noremap = true, silent = true, desc='Reset git hunk'})
 
+-- MY USER COMMANDS
+
+vim.api.nvim_create_user_command('Format', function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ['end'] = { args.line2, end_line:len() },
+    }
+  end
+  require('conform').format { async = true, lsp_fallback = true, range = range }
+end, { range = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
