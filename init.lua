8699e7babb6bef77fcddf0071058928f1e5a0ac9
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2024-04-18 21:13:57 -0400

    Add keymap to center line when moving up/down by half a page

--- init.lua
+++ init.lua
@@ -202,6 +202,8 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
 -- vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle [U]ndo tree' })
 
 vim.keymap.set('x', '<leader>p', [["_dP]])
