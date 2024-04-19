1b02a2f50dc06eb9905112b10f36eaeae3366afe
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2024-04-18 21:17:36 -0400

    Update keymap for formatting current file.

--- init.lua
+++ init.lua
@@ -239,7 +239,7 @@ vim.api.nvim_set_keymap('n', '<C-g>r', '<cmd>lua reset_hunk()<CR>', { noremap = 
 
 -- MY USER COMMANDS
 
-vim.api.nvim_create_user_command('Format', function(args)
+vim.api.nvim_create_user_command('F', function(args)
   local range = nil
   if args.count ~= -1 then
     local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
