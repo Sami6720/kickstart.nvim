7080358871266b61cb62242329e889aa5c15f2a5
sami6720 <saminur67@gmail.com>
2024-08-11 14:20:16 -0400

    Update tree sitter ensure installed to incude Java and Python

--- init.lua
+++ init.lua
@@ -884,7 +884,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'java', 'python'},
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
