a4cba8419ab304f3a62df11245356ba5b796d118
sami6720 <saminur67@gmail.com>
2024-08-29 14:20:56 -0400

    Add java debug stuff

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,10 @@
 local config = {
   cmd = { vim.fn.expand '~/.local/share/nvim/mason/bin/jdtls' },
   root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+
+  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+  init_options = {
+    bundles = { vim.fn.glob('/home/sami6720/.local/lib/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar', 1) },
+  },
 }
 require('jdtls').start_or_attach(config)
