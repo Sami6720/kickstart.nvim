f3b304f135e670f0f6e72affef3d139894a5635d
sami6720 <saminur67@gmail.com>
2024-10-16 13:46:30 -0400

    Add junit jar files to path

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -6,5 +6,16 @@ local config = {
   init_options = {
     bundles = { vim.fn.glob('/home/sami6720/.local/lib/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar', 1) },
   },
+
+  settings = {
+    java = {
+      project = {
+        referencedLibraries = {
+          '/home/sami6720/myprojs/comp_fall2024/chapter5/lib/junit-4.13.2.jar',
+          '/home/sami6720/myprojs/comp_fall2024/chapter5/lib/hamcrest-core-1.3.jar',
+        },
+      },
+    },
+  },
 }
 require('jdtls').start_or_attach(config)
