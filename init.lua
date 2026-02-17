17cfbfcc7022ac7947520496e10996dca5eb668e
Mohammad Sami Nur Islam <saminur@rorqual2.rorqual.calcul.quebec>
2026-02-17 14:21:25 -0500

    update init.lua

--- init.lua
+++ init.lua
@@ -1205,8 +1205,8 @@ require('lazy').setup({
     ---@type oil.SetupOpts
     opts = {},
     -- Optional dependencies
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
   },
